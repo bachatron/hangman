1dd19fba6f3bce4826effada29d45a949b8f6515
IAIR <idz420@gmail.com>
2023-04-12 22:32:37 -0300

    Game and Player class added.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -17,3 +17,34 @@ def check_letter(letter)
 end
 
 check_letter('e')
+
+while $player_word.include?('_')
+  Array('a'..'z').each {|letter| check_letter(letter)}
+end
+
+class Player
+  
+  def initialize
+    @lives = 15
+  end
+
+  def select_letter
+    print "Select a valid letter: "
+    loop do
+      letter = gets.chomp until letter.length == 1 && Array('a'..'z').include(letter)
+    end
+  end
+
+  def check_letter(letter, game_word)
+    game_word.each_with_index do |x, i|
+      game_word[i] = letter if x == letter
+    end
+    puts game_word.join(' ')
+  end
+
+end
+
+class Game
+
+  def initialize
+    word = random_word
